ce74e01cfcbbcd50de9f75868e3208e5e759c9b6
David-Lisboa <lisboa.prog@gmail.com>
2021-02-18 20:45:28 -0400

    function Board criado

--- src/index.js
+++ src/index.js
@@ -2,14 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+function Board(){
+    return(
+        <div>
+            <div className="board-row">
+                <button className = "square">X</button>
+                <button className = "square">X</button>
+                <button className = "square">X</button>
+            </div>
+            <div className="board-row">
+                <button className = "square">X</button>
+                <button className = "square">X</button>
+                <button className = "square">X</button>
+            </div>
+            <div className="board-row">
+                <button className = "square">X</button>
+                <button className = "square">X</button>
+                <button className = "square">X</button>
+            </div>
+        </div>
+    );
+}
+
+
 //criação do componnete Game
 function Game(){
-    return <div>Jogo</div>
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board/>
+            </div>
+
+            <div className="game-info">
+                Info
+
+            </div>
+
+        </div>
+    );
 }
 
 ReactDOM.render(
     //Elemento que sera rederizado
-    <Game></Game>,
+    <Game/>,
     //Local que esse lemento sera renderizado
     document.getElementById('root')
 );
